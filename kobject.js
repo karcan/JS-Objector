581e6cae10e051d1dd8ff720d8b5659585b9ea2d
Karcan Ozbal <karcanozbal@hotmail.com.tr>
2021-01-29 12:58:29 +0300

    Create kobject.js
    let's begin..

--- kobject.js
+++ kobject.js
@@ -1,5 +1,40 @@
 (function(global){
 
+    class kObject {
+        constructor(object){
+            return new kObject.ct(object);
+        }
+    }
+
+    kObject.ct = class {
+        constructor(object){
+            this._object = object;
+            return this.resolve();
+        }
+        resolve(){
+            return new kObject.new_object(this._object);
+        }
+    }
+
+    kObject.new_object = class {
+        constructor(object){
+            this.__data = {
+                object : object,
+                properties : {}
+            };
+        }
+        get(){
+            return this;
+        }
+    }
+
+    kObject.list_object = class {
+        constructor(object){
+            this.__data = {
+                object : object
+            };
+        }
+    }
 
     global.kObject = global.$kO = kObject;
 }(window));
