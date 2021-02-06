1dba080a9de2cdddae19799b4e7a5d6a6dc48e99
Karcan Ozbal <karcanozbal@hotmail.com.tr>
2021-02-06 22:58:26 +0300

    ..

--- docs/main.js
+++ docs/main.js
@@ -1,11 +1,18 @@
 (function(){
     const params = new URLSearchParams(window.location.search);
     const page = params.get('page');
-    fetch('route.json')
+    fetch('route.json', {
+
+    })
     .then(response => response.json())
     .then(data => {
         let html = data.find(i=>i.route == page || "main");
-        console.log(html);
+
+        fetch("pages/" + html.html)
+        .then(response => {
+            document.querySelector("#main").innerHTML(response);
+        });
+
     });
 })();
 
