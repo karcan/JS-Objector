9fef583ccf5f5fbed900ecc4319ececabdd3c398
Karcan Ozbal <karcanozbal@hotmail.com.tr>
2021-02-06 23:08:07 +0300

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -2,16 +2,15 @@
     const params = new URLSearchParams(window.location.search);
     const page = params.get('page');
     console.log("page " , page);
-    fetch('route.json', {
-
-    })
+    fetch('route.json')
     .then(response => response.json())
     .then(data => {
-        let html = data.find(i=>i.route == page ?? "main");
+        let html = data.find(i=>i.route == page);
         console.log("html " , html);
-        fetch("pages/" + html.html + "?v=" + Math.random())
+        fetch("pages/" + html.html || "main" + "?v=" + Math.random())
         .then(response => {
-            document.querySelector("#main").innerHTML(response);
+            console.log("response " , response);
+            document.querySelector("#main").innerHTML = response;
         });
 
     });
